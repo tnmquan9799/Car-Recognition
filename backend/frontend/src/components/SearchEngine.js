3d3e573d3dceb6dc77ce40ab4f26b2dac220b178
Nguyễn Trần Hữu Tuấn <tuannguyencorei7@gmail.com>
2021-06-15 22:01:06 +0700

    correct syntax

--- backend/frontend/src/components/SearchEngine.js
+++ backend/frontend/src/components/SearchEngine.js
@@ -4,6 +4,7 @@ import Grid from '@material-ui/core/Grid';
 // axios.defaults.xsrfCookieName = 'csrftoken'
 // axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 const download = require('image-downloader')
+
 class SearchEngine extends Component {
   constructor(props) {
     super(props)
@@ -20,32 +21,44 @@ class SearchEngine extends Component {
       url: file,
       dest: '../../../save-image'
     }
-    download.image(options)
-        .then(({dest})) => {
-          console.log('Saved to', dest)
-    }).catch((err) => console.log(err))
+    download.image(options).then(function () {
+      console.log('SUCCESS!!');
+    })
+      .catch(function () {
+        console.log('FAILURE!!');
+      });
+    }
+    //   options = {
+    //     url: file,
+    //     dest: '../../../save-image'
+    //   }
+    //   download.image(options)
+    //       .then(({dest})) => {
+    //         console.log('Saved to', dest)
+    //   })
+    //       .catch((err) => console.log(err))
+    // }
+    render() {
+      return (
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justify="center"
+          style={{ minHeight: '100vh' }}
+        >
+
+          <input type="file" onChange={this.handleChange} />
+          <hr />
+          <img src={this.state.file}
+            style={{
+              width: "100%",
+            }} />
+
+        </Grid >
+      );
+    }
   }
-  render() {
-    return (
-      <Grid
-        container
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justify="center"
-        style={{ minHeight: '100vh' }}
-      >
 
-        <input type="file" onChange={this.handleChange} />
-        <hr />
-        <img src={this.state.file}
-          style={{
-            width: "100%",
-          }} />
-
-      </Grid >
-    );
-  }
-}
-
-export default SearchEngine;
+  export default SearchEngine;
