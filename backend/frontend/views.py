c3f98942aa1fc250d8c45232d685f88442d8411f
tnmquan9799 <tnmquan9799@gmail.com>
2021-06-20 23:59:14 +0700

    update upload img_name
    workin on calling demo.py to run demo form received image

--- backend/frontend/views.py
+++ backend/frontend/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from django.conf import settings
+from subprocess import call
 import importlib
 from django.views.decorators.csrf import ensure_csrf_cookie
 import cgi
@@ -18,6 +19,14 @@ def save_file(request):
     SAVED_PATH = "../test-folder/"
     fileitem = request.FILES['file']
     filename = request.FILES['file'].name
+    file_extension = filename.split('.')[1]
+    filename = "001." + file_extension
+    folderpath = "test-folder/001." + file_extension
     fn = os.path.basename(filename)
     open(SAVED_PATH + fn, 'wb').write(fileitem.file.read())
+    # call(["python", "../../../demo.py", folderpath])
+    # call(["python", "/cut_string.py"])
+    # print("Running demo")
     return HttpResponse('The file "' + fn + '" was uploaded successfully')
+
+
