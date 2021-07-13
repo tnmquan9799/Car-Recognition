0056affae59ae1aafe1bdd1327f79cca455664f6
Nguyễn Trần Hữu Tuấn <tuannguyencorei7@gmail.com>
2021-07-13 13:24:58 +0700

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-@7_a7@p8!cijn)s3$uh1+$a3lvs6=hbael$hi@g3c)fll79d04'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["car-recognition-projectapp.herokuapp.com"]
+ALLOWED_HOSTS = ["car-recognition-projectapp.herokuapp.com","127.0.0.1:8000"]
 
 
 # Application definition
@@ -84,7 +84,7 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
+        'NAME':  os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
