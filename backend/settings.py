6b0ad63d22b4ac18c1bdbd5ad7b5d4555461e3f1
Nguyễn Trần Hữu Tuấn <tuannguyencorei7@gmail.com>
2021-07-14 07:17:25 +0700

    update

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-@7_a7@p8!cijn)s3$uh1+$a3lvs6=hbael$hi@g3c)fll79d04'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["car-recognition-projectapp.herokuapp.com","127.0.0.1:8000"]
 
